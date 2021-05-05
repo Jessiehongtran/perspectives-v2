19a480464fe094454d5af682679142fb73a0c20e
Hong Tran <jessietran@fb.com>
2021-05-05 14:50:27 -0700

    got text shown with bg

--- charIntro2.js
+++ charIntro2.js
@@ -7,15 +7,32 @@ const charPos = {
 
 const bgStartPos = {
     x: 90,
-    y: 20
+    y: 30
 }
+
+let textIsBeingShown = false
 
 const scenes = [
     {
         img: "https://res.cloudinary.com/dfulxq7so/image/upload/v1620182890/imageedit_6_9313307156_pfysfl.png",
+        w: 50,
+        h: 22,
+        text: "from China",
+        showText: false
     },
     {
-        img: "https://res.cloudinary.com/dfulxq7so/image/upload/v1620186269/imageedit_8_2296018956_a5n6ek.png"
+        img: "https://res.cloudinary.com/dfulxq7so/image/upload/v1620186269/imageedit_8_2296018956_a5n6ek.png",
+        w: 10,
+        h: 12,
+        text: "work as a developer",
+        showText: false
+    },
+    {
+        img: "https://www.pinclipart.com/picdir/big/449-4493955_use-your-7-day-free-trial-to-build.png",
+        w: 20,
+        h: 22,
+        text: "in a tech company",
+        showText: false
     }
 ]
 
@@ -24,18 +41,19 @@ let preBG
 for (let i = 0; i < scenes.length; i++){
     let bg = document.createElement('img')
     bg.setAttribute('class', 'bgImage')
-    bg.style.width = '100%'
-    bg.style.maxWidth = '600px'
     bg.style.position = 'absolute'
     bg.src = scenes[i].img
     if (i > 0){
-        scenes[i].x = scenes[i-1].x + Math.floor(preBG.clientWidth/window.screen.width*100) + 20
+        scenes[i].x = scenes[i-1].x + scenes[i-1].w + 15
     } else {
         scenes[i].x = bgStartPos.x
     }
+    scenes[i].originX = scenes[i].x 
     scenes[i].y = bgStartPos.y
     bg.style.left = `${scenes[i].x}%`
-    bg.style.top = `${scenes[i].y}%`
+    bg.style.bottom = `${scenes[i].y}%`
+    bg.style.width = `${scenes[i].w}%`
+    bg.style.height = `${scenes[i].h}%`
     container.appendChild(bg)
     preBG = bg
 }
@@ -44,6 +62,8 @@ character.style.left = `${charPos.x}%`
 character.style.top = `${charPos.y}%`
 
 let walkInd = 0
+let curTextEl 
+let curTextY
 
 
 function getCharacterWalkStep(){
@@ -62,6 +82,7 @@ function getCharacterWalkStep(){
 
 let changeX
 
+
 function handleKeyDown(e){
     let bgImages = document.getElementsByClassName('bgImage')
     if (e.key === "d"){
@@ -75,9 +96,37 @@ function handleKeyDown(e){
     for (let i = 0; i< scenes.length; i++){
         scenes[i].x += changeX
         bgImages[i].style.left = `${scenes[i].x}%`
-        bgImages[i].style.top = `${scenes[i].y}%`
+        bgImages[i].style.bottom = `${scenes[i].y}%`
+        let flexNum = 50
+        if (!textIsBeingShown && !scenes[i].showText && scenes[i].x < scenes[i].originX - scenes[i].w - i*flexNum){
+            curTextEl = document.createElement('div')
+            curTextEl.style.padding = '8px 20px'
+            curTextEl.style.borderRadius = '8px'
+            curTextEl.style.backgroundColor = 'white'
+            curTextEl.style.color = 'black'
+            curTextEl.style.position = 'absolute'
+            curTextEl.style.left = `${ scenes[i].x }%`
+            curTextEl.style.top = `${ scenes[i].y }%`
+            curTextEl.innerHTML = scenes[i].text
+            curTextY = scenes[i].y
+            container.appendChild(curTextEl)
+            showText()
+            scenes[i].showText = true
+        }
     }
     getCharacterWalkStep()
+}
+
+function showText(){
+    if (curTextY > -10){
+        curTextY -= 5
+        curTextEl.style.top = `${ curTextY }%`
+        textIsBeingShown = true
+        setTimeout(showText, 400)
+    } else {
+        textIsBeingShown = false
+    }
+    
 }
 
 document.addEventListener('keydown', handleKeyDown)
